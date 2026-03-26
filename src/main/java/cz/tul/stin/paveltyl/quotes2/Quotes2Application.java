6571c7d9647fc29a18d289abfb71a6b29ac02f1e
Pavel Tyl <pavel.tyl@gmail.com>
2026-03-26 14:29:29 +0100

    Initial commit

---
+++ src/main/java/cz/tul/stin/paveltyl/quotes2/Quotes2Application.java
@@ -0,0 +1,13 @@
+package cz.tul.stin.paveltyl.quotes2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Quotes2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Quotes2Application.class, args);
+    }
+
+}
---
+++ src/test/java/cz/tul/stin/paveltyl/quotes2/Quotes2ApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.tul.stin.paveltyl.quotes2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Quotes2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
